ee4369d64090273e3a7be90ffa7573a525c75e91
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-01-25 16:24:47 -0300

    exercicio ajustado e funcional

--- fundamentos/bloco-01-unix-e-shell/unix-shell-bash-parte-2/.parte_1.sh
+++ fundamentos/bloco-01-unix-e-shell/unix-shell-bash-parte-2/.parte_1.sh
@@ -8,29 +8,16 @@ cd unix_tests
 #criando um novo arquivo e adicionando linhas
 touch skills2.txt
 
-x = "Internet"
-y = "Unix"
-z= "Bash"
-
-echo ?x >> skills2.txt
-echo ?y >> skills2.txt
-echo ?z >> skills2.txt
+echo -e "Internet\nUnix\nBash" >> skills2.txt
 
 #escrever mais 5 linhas
-a = "Html"; b= "CSS"; c= "JavaScript"; d= "Soft Skills"; e= "Java"
-
-echo ?a >> skills2.txt
-echo ?b >> skills2.txt
-echo ?c >> skills2.txt
-echo ?d >> skills2.txt
-echo ?e >> skills2.txt
+echo -e "Html\nCSS\nJavascript\nSoft Skills\nJava">> skills2.txt
 
 #contar linhas do arquivo
 
 wc -l skills2.txt
 
 #criar um arquivo que representa as três primeiras linhas em ordem alfabética.
-
-head -3 |sort < skills2.txt | cat >> top_skills.txt
+sort < skills2.txt |head -n 3| cat >> top_skills.txt
 
 #continua...
