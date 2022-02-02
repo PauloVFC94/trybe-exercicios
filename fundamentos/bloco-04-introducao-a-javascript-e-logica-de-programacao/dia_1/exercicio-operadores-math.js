b8592d881e755a780eea2a6101f0778d19ae97c9
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-02-02 18:41:25 -0300

    exercicio 1 operadores math

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_1/exercicio-operadores-math.js
@@ -0,0 +1,16 @@
+let a;
+let b;
+a = 10;
+b = 32;
+
+let soma = a + b;
+let subtracao = a - b;
+let mult = a * b;
+let divisao = a / b;
+let resto = a % b;
+
+console.log("soma: " + soma );
+console.log("subtração: " + subtracao );
+console.log("multiplicação: " + mult );
+console.log("divisão: " + divisao );
+console.log("resto: " + resto );
