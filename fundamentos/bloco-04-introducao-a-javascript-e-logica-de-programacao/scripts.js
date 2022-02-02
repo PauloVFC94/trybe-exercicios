285fa6e410b8eed4c810aa82f27ef3dcdd46f9e2
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-02-02 15:35:46 -0300

    pasta javascript + 1º exercicio

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Paulo";
+const birthCity = "Nova Iguaçu";
+let birthYear = 1994;
+console.log(myName,birthCity,birthYear);
+birthYear = 2030;
+console.log(birthYear);
+/*birthCity = "Belford Roxo";*/
+console.log(birthCity);
