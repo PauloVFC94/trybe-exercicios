c0a9dbb06f24d4d81c13cfa732a728ee02eae30d
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-02-03 17:08:27 -0300

    exercicios 8 e 9

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_2/exercicio-8-e-9.js
@@ -0,0 +1,9 @@
+let vetor =[];
+for (let index = 0;index < 25; index += 1){
+    vetor[index] = index + 1; 
+} // exercicio 8
+console.log(vetor);
+
+for (let index = 0; index < vetor.length ; index += 1){
+    console.log(vetor[index] + " dividido por 2 é igual a " + (vetor[index]/2) + ".");
+} // exercicio 9
