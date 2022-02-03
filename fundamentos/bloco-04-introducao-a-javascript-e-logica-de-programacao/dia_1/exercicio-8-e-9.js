ef538eb36ccce1962f854021786ee180de0d9a16
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-02-03 13:31:23 -0300

    exercicio 8 e 9

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_1/exercicio-8-e-9.js
@@ -0,0 +1,17 @@
+const num1 = 10;
+const num2 = 30;
+const num3 = 60;
+
+if (num1 % 2 == 0 ||num2 % 2 == 0 ||num3 % 2 == 0){ //par
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+if (num1 % 2 == 1 ||num2 % 2 == 1 ||num3 % 2 == 1){ //impar
+    console.log("true");
+}
+else{
+    console.log("false");
+}
