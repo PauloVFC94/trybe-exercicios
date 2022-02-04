38db8fab46fe79f1420cb9a9dcd65ecfa6f9167d
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-02-04 19:53:23 -0300

    correçao no codigo quem e mais novo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/algoritimo-quem-e-mais-novo.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia_3/algoritimo-quem-e-mais-novo.js
@@ -4,11 +4,25 @@
 let pessoas = ["Carolzita","Murilo","Baêta"];
 let idades =[Math.floor(Math.random() * 110),Math.floor(Math.random() * 110),Math.floor(Math.random() * 110)];
 console.log("A idade de Carolzita, Murilo e Baêta são, respectivamente: " +idades);
-let menorIdade = idades[0];
+let menorIdade = Infinity;
+let novos = [];
 for (let index=0;index < idades.length; index +=1){
-    if(menorIdade > idades[index] ){
+    if(idades[index] < menorIdade){
         menorIdade = idades[index];
     }
 }
+for (let index=0; index <idades.length; index +=1){ //Situação de ter pessoas com idades iguais
+    if(menorIdade === idades[index]){
+        novos.push(index);
+    }
+}
 
-console.log(pessoas[idades.indexOf(menorIdade)] + " é a pessoa mais nova.");
+if(novos.length === 1){ //imprimir o nome nas diversas situações.
+console.log(pessoas[novos[0]] + " é a pessoa mais nova.");
+}
+else if (novos.length === 2){
+    console.log(pessoas[novos[0]] + " e " +pessoas[novos[1]] + " são as pessoas mais novas.");
+}
+else if(novos.length === 3){
+    console.log(pessoas[novos[0]] + ", " +pessoas[novos[1]] + " e " + pessoas[novos[2]] + " são as pessoas mais novas.");
+}
