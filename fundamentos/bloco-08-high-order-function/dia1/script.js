e696e0784c12941c4a2f5347803f02d2359fb6b1
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-03-10 13:32:51 -0300

    Exercicio 3

--- fundamentos/bloco-08-high-order-function/dia1/script.js
+++ fundamentos/bloco-08-high-order-function/dia1/script.js
@@ -34,3 +34,30 @@ const sorteio = (numero, callback) => {
     return callback(numero);
 }
 console.log(sorteio(3, verificaSorteio));
+
+/* 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+*/
+const corrigeProva = (gabarito, respostas) => {
+    const gabaritoProva = gabarito;
+    const respostaAluno = respostas;
+    let pontuacao = 0;
+    for (let i in gabaritoProva) {
+        if (respostaAluno[i] === 'N.A') {
+            pontuacao += 0;
+        }
+        else if (respostaAluno[i] === gabaritoProva[i]) {
+            pontuacao += 1;
+        }
+        else if (respostaAluno[i] !== gabaritoProva[i]) {
+            pontuacao -= 0.5;
+    }
+}
+    return `A pontuação do Aluno na prova foi ${pontuacao} pontos.`
+}
+
+const confereProva = (gabarito, respostas, callback) => {
+    return callback (gabarito, respostas);
+}
+const respostasProva = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const respostaAluno = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+console.log(confereProva(respostasProva, respostaAluno, corrigeProva));
